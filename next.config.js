4eccca414793d78351825a9e6e197e237e80243e
rajput-hemant <rajput.hemant2001@gmail.com>
2024-08-12 00:34:00 +0530

    feat: add src directory
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./lib/env.js");
+await import("./src/lib/env.js");
 
 const isProd = process.env.NODE_ENV === "production";
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
 
 const config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     container: {
       center: true,
